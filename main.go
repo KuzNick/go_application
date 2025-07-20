6d06d1a06443046f5e909cb33fa6cc8719fa027a
baracuda1989 <n9858508501@gmail.com>
2025-07-20 20:25:42 +0300

    05.04 Свой ServeMux

--- main.go
+++ main.go
@@ -6,26 +6,35 @@ import (
 )
 
 /*
-	Обязательные 2 аргумента: (w http.ResponseWriter, req *http.Request)
-	w http.ResponseWriter - куда будем писать ответ
-	req *http.Request - указатель на hhtp request (запрос, который пришёл изначально)
+Обязательные 2 аргумента: (w http.ResponseWriter, req *http.Request)
+w http.ResponseWriter - куда будем писать ответ
+req *http.Request - указатель на hhtp request (запрос, который пришёл изначально)
 */
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Hello")
 }
 
-
 func main() {
+	/*
+		Собстенный ServeMux
+	*/
+	router := http.NewServeMux()
 	/*
 		Обработчик
 	*/
-	http.HandleFunc("/hello", hello) // если первм агрументом ничего не передавать, будут обрабатываться толкько GET запросы
+	router.HandleFunc("/hello", hello) // если первм агрументом ничего не передавать, будут обрабатываться толкько GET запросы
+
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: router,
+	}
+
 	fmt.Println("Server is listening on port 8081")
 
 	/*
-		Случшает все коннекшены по tcp по определенном у порту и адресу и передавать 
+		Случшает все коннекшены по tcp по определенном у порту и адресу и передавать
 		их по определенному роутингу, который мы так же можем задавать
-	*/ 
-	http.ListenAndServe(":8081", nil) //ипользуем дефолтный ServerMux
+	*/
+	server.ListenAndServe() //ипользуем дефолтный ServerMux
 
 }
